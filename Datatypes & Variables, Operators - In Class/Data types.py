8913a8207c05e4f1f2e96ec8620040f7500529eb
MohammedImdadS <69243787+MohammedImdadS@users.noreply.github.com>
2022-03-08 19:50:54 +0530

    Create Data types.py

---
+++ Datatypes & Variables, Operators - In Class/Data types.py
@@ -0,0 +1,37 @@
+# #Problem Statement
+# Some Data types are given below:-
+# Integer
+# Long
+# float
+# Double
+# char
+# Your task is to take input in the given format and print them in the same order.
+# Input
+# The input contains the following values separated by lines:- integer, Long, float, double, char
+# Output
+# Print each element in a new line in the same order as input.
+# Example
+# Sample Input:-
+# 2
+# 2312351235
+# 1.21
+# 543.1321
+# c
+
+# Sample Output:-
+# 2
+# 2312351235
+# 1.21
+# 543.1321
+# c
+a = int(input())
+b = int(input())
+c = float(input())
+d = float(input())
+e = input()
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
